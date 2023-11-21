73111bb0581109e9036036e48f1a62f5536f71f9
Riadh Laabidi <riadh.by.laabidi@gmail.com>
2023-11-21 20:43:19 +0100

    Add project pages to sitemap (#11)

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,9 +1,13 @@
-import { MetadataRoute } from 'next'
+import { allProjects } from 'contentlayer/generated';
+import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const routes = ['', '/projects', '/contact', '/blog'].map(route => ({
+  const routes = ['', '/projects', '/contact', '/blog'];
+
+  allProjects.forEach(project => routes.push(`/projects/${project.slug}`));
+
+  return routes.map(route => ({
     url: `https://riadh.me${route}`,
     lastModified: new Date().toISOString().split('T')[0],
-  }))
-  return [...routes]
+  }));
 }
