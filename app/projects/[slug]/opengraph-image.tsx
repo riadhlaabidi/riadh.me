b227932684b9d2f91080fa05dc9f780a69266da0
Riadh Laabidi <riadh.by.laabidi@gmail.com>
2023-11-23 19:54:29 +0100

    Add og image for projects (#10)

--- app/projects/[slug]/opengraph-image.tsx
+++ app/projects/[slug]/opengraph-image.tsx
@@ -1,7 +1,7 @@
 import { Project, allProjects } from 'contentlayer/generated';
 import { ImageResponse } from 'next/og';
 
-export const alt = 'Project thumbnail';
+export const runtime = 'edge';
 
 export function generateImageMetadata({
   params,
@@ -10,13 +10,60 @@ export function generateImageMetadata({
 }) {
   const metadata = {
     contentType: 'image/png',
+    id: 'og-image',
   };
   const project = allProjects.find((p: Project) => p.slug === params.slug);
   return project
-    ? { ...metadata, alt: `${project.name} project thumbnail` }
-    : metadata;
+    ? [{ ...metadata, alt: `${project.name} project thumbnail` }]
+    : [metadata];
 }
 
 export default async function Image({ params }: { params: { slug: string } }) {
-  return new ImageResponse(<div></div>, {});
+  const project = allProjects.find((p: Project) => p.slug === params.slug);
+
+  if (!project) {
+    return null;
+  }
+
+  const font = fetch(
+    new URL('../../../public/fonts/Raleway-Regular.ttf', import.meta.url),
+  ).then(res => res.arrayBuffer());
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          backgroundImage: 'url(http://localhost:3000/images/og-bg.png)',
+          display: 'flex',
+          flexDirection: 'column',
+          width: '100%',
+          height: '100%',
+          alignItems: 'flex-start',
+          justifyContent: 'center',
+          padding: 210,
+        }}
+      >
+        <h2
+          style={{
+            fontFamily: 'Geist',
+            color: '#fff',
+            fontSize: 100,
+          }}
+        >
+          <span>{project.name}</span>
+        </h2>
+      </div>
+    ),
+    {
+      width: 1920,
+      height: 1080,
+      fonts: [
+        {
+          name: 'Geist',
+          data: await font,
+          style: 'normal',
+        },
+      ],
+    },
+  );
 }
