cc547cd4669444b7104732f9bb113eb0eca05e08
Riadh Laabidi <riadh.by.laabidi@gmail.com>
2024-06-26 09:34:43 +0100

    Fix contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -32,72 +32,72 @@ export default function Contact() {
         </p>
         <p>Instead, you can send me a quick message using the form below:</p>
       </div>
-      <form
-        action={sendAction}
-        className="flex flex-col items-start lg:w-1/2 mt-8"
-      >
-        <div className="mt-3 flex w-full after:content-[']'] before:content-['['] before:text-dark5 after:text-dark5 focus-within:after:text-orange focus-within:before:text-orange">
-          <label htmlFor="name" className="sr-only">
-            Name:
-          </label>
-          <input
-            className="caret-orange placeholder-dark5 placeholder:font-light bg-transparent border-none outline-none py-0 px-1 w-full"
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Name..."
-            required
-          />
-        </div>
-        {state.errors?.name && (
-          <small className="text-sm font-light text-red px-4">
-            {state.errors.name}
-          </small>
-        )}
-        <div className="mt-3 flex w-full after:content-[']'] before:content-['['] before:text-dark5 after:text-dark5 focus-within:after:text-orange focus-within:before:text-orange">
-          <label htmlFor="email" className="sr-only">
-            Email:
-          </label>
-          <input
-            className="caret-orange placeholder-dark5 placeholder:font-light bg-transparent border-none outline-none py-0 px-1 w-full"
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Email address..."
-            required
-          />
-        </div>
-        {state.errors?.email && (
-          <small className="text-sm font-light text-red px-4">
-            {state.errors.email}
-          </small>
-        )}
-        <div className="mt-3 flex w-full after:content-[']'] before:content-['['] before:text-dark5 after:text-dark5 focus-within:after:text-orange focus-within:before:text-orange after:flex after:items-end">
-          <label htmlFor="message" className="sr-only">
-            Message:
-          </label>
-          <textarea
-            className="caret-orange max-h-[40vh] resize-none placeholder-dark5 placeholder:font-light bg-transparent border-none outline-none py-0 px-1 w-full"
-            id="message"
-            name="message"
-            rows={6}
-            placeholder="Your message..."
-            required
-          ></textarea>
-        </div>
-        {state.errors?.email && (
-          <small className="text-sm font-light text-red px-4">
-            {state.errors.message}
-          </small>
-        )}
-        {state.sent ? (
-          <p className="text-green mt-8">
-            Thanks for reaching out, I will respond shortly.
-          </p>
-        ) : (
+
+      {state.sent ? (
+        <p className="text-green mt-7">
+          Thanks for reaching out, I will respond shortly.
+        </p>) : (
+        <form
+          action={sendAction}
+          className="flex flex-col items-start lg:w-1/2 mt-8"
+        >
+          <div className="mt-3 flex w-full after:content-[']'] before:content-['['] before:text-dark5 after:text-dark5 focus-within:after:text-orange focus-within:before:text-orange">
+            <label htmlFor="name" className="sr-only">
+              Name:
+            </label>
+            <input
+              className="caret-orange placeholder-dark5 placeholder:font-light bg-transparent border-none outline-none py-0 px-1 w-full"
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Name..."
+              required
+            />
+          </div>
+          {state.errors?.name && (
+            <small className="text-sm font-light text-red px-4">
+              {state.errors.name}
+            </small>
+          )}
+          <div className="mt-3 flex w-full after:content-[']'] before:content-['['] before:text-dark5 after:text-dark5 focus-within:after:text-orange focus-within:before:text-orange">
+            <label htmlFor="email" className="sr-only">
+              Email:
+            </label>
+            <input
+              className="caret-orange placeholder-dark5 placeholder:font-light bg-transparent border-none outline-none py-0 px-1 w-full"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email address..."
+              required
+            />
+          </div>
+          {state.errors?.email && (
+            <small className="text-sm font-light text-red px-4">
+              {state.errors.email}
+            </small>
+          )}
+          <div className="mt-3 flex w-full after:content-[']'] before:content-['['] before:text-dark5 after:text-dark5 focus-within:after:text-orange focus-within:before:text-orange after:flex after:items-end">
+            <label htmlFor="message" className="sr-only">
+              Message:
+            </label>
+            <textarea
+              className="caret-orange max-h-[40vh] resize-none placeholder-dark5 placeholder:font-light bg-transparent border-none outline-none py-0 px-1 w-full"
+              id="message"
+              name="message"
+              rows={6}
+              placeholder="Your message..."
+              required
+            ></textarea>
+          </div>
+          {state.errors?.message && (
+            <small className="text-sm font-light text-red px-4">
+              {state.errors.message}
+            </small>
+          )}
           <SubmitButton />
-        )}
-      </form>
+        </form>
+      )}
     </>
   );
 }
