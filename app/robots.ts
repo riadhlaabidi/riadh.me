5f226ea4ffa403d15511e6d7da7a82f30a6e76c9
Riadh Laabidi <riadh.by.laabidi@gmail.com>
2024-06-26 20:38:07 +0100

    Update robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,24 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-    },
-    sitemap: 'https://riadh.me/sitemap.xml',
-    host: 'https://riadh.me',
+    rules: [
+      {
+        userAgent: 'Googlebot',
+        allow: "/",
+        disallow: ['/files/', '/og*'],
+      },
+      {
+        userAgent: 'AdsBot-Google',
+        disallow: '/',
+      },
+      {
+        userAgent: '*',
+        disallow: '/'
+      }
+    ],
+    sitemap: [
+      'https://riadh.me/sitemap.xml',
+      'https://www.riadh.me/sitemap.xml'
+    ],
   }
 }
